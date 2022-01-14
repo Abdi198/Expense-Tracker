bf3a95e0c2d488670d3a889202ed1f5889485b60
Abdi Ismail <sessiano@gmail.com>
2022-01-14 16:26:09 -0600

    fix: rendered expense list dynamically

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,15 +4,7 @@ import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 
-const ExpenseItem = (props) => {
-
-     const [title, setTitle] = useState(props.title);
-
-    const clickHandler = () => {
-         setTitle('Updated');
-         console.log(title);
-    
-    }
+const ExpenseItem = (props) => {  
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
@@ -20,7 +12,6 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>);
 }
 
